82dad00efc0034777441277a20026ccf8741b1a4
Oleg <bezrukov-oa@nm.ru>
2021-05-23 18:19:00 +0300

    create parser

--- test_parse/test_parse/pipelines.py
+++ test_parse/test_parse/pipelines.py
@@ -6,8 +6,10 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import json
 
 class TestParsePipeline:
     def process_item(self, item, spider):
+        with open(f'test_parse/files/{item["id"][0]}.json','w', encoding='utf-8') as f:
+            f.write(json.dumps(item,ensure_ascii=False))
         return item
